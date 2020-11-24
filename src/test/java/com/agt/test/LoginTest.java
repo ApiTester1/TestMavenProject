eb57ae3c5f01da184b38a626658756c34876980e
samsher <samsher@DESKTOP-0H65ON5>
2020-11-24 13:22:45 +0530

    Initial Commit

---
+++ src/test/java/com/agt/test/LoginTest.java
@@ -0,0 +1,35 @@
+package com.agt.test;
+
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	public void positiveLoginTest() {
+		System.out.println("LoginTest.positiveLoginTest()");
+	}
+
+	@Test
+	public void negativeTest() {
+		System.out.println("LoginTest.negativeTest()");
+	}
+
+	@Test
+	public void failedTest() {
+		
+		Assert.assertTrue(false);
+	}
+
+	@Test
+	public void skipTest() {
+		
+		new SkipException("Test skipped");
+	}
+	
+	
+	
+	
+	
+}
